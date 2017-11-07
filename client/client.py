28fae536c9ae6ebb2a3af1d5e750d86d7f624d5e
Hugo Genesse <svieg.ashcasce@gmail.com>
2017-11-06 23:02:01 -0500

    client is now a class and generates password

--- client/client.py
+++ client/client.py
@@ -1,25 +1,67 @@
 import telnetlib
 import json
+import secrets
+import string
 
-with open("devices.json", "r") as devices_list:
-    devices = json.loads(devices_list.read())
+class Aiakos(object):
+
+    def __init__(self):
+        self.devices = None
+        self.users = None
+        self.tn = None
+
+    def get_devices(self, filename):
+
+        with open(filename, "r") as devices_list:
+            self.devices = json.loads(devices_list.read())
+
+    def connect_telnet(self, user):
+        password = self.users[username]
+        self.tn = telnetlib.Telnet(ip)
+
+        self.tn.read_until(b"Login: ")
+        self.tn.write(username.encode('ascii') + b"\n")
+        self.tn.read_until(b"Password: ")
+        self.tn.write(password.encode('ascii') + b"\n")
+
+    def generate_password(self):
+        
+        alphabet = string.ascii_letters + string.digits
+        while True:
+            password = ''.join(choice(alphabet) for i in range(10)) # TODO: custom length
+            if (any(c.islower() for c in password)
+                and any(c.isupper() for c in password)
+                and sum(c.isdigit() for c in password) >= 3): # TODO: custom number length
+        break
+        return password
+
+    def change_password(self, new_password):
+
+        self.tn.write(b"passwd\n")
+        self.tn.read_until(b"New password:")
+        self.tn.write(new_password.encode("ascii") + b"\n")
+        self.tn.write(b"exit\n")
+
+    def flash_device(self, ip):
+
+        print("Flashing: {} ".format(ip))
+
+        self.users = self.devices[ip]
+        for username in users.keys():
+            connect_telnet(username)
+            new_password = generate_password()
+            change_password(new_password)
+            print(self.tn.read_all().decode('ascii'))
+
+    def run():
+
+        get_devices("devices.json")
+
+        for ip in devices.keys():
+            flash_device(ip)
+
 
 for ip in devices.keys():
-    print("Flashing: {} ".format(ip))
-
-    users = devices[ip]
-    for username in users.keys():
-        password = users[username]
-        tn = telnetlib.Telnet(ip)
-
-        tn.read_until(b"Login: ")
-        tn.write(username.encode('ascii') + b"\n")
-        if password:
-            tn.read_until(b"Password: ")
-        tn.write(password.encode('ascii') + b"\n")
-
-        tn.write(b"sh\n")
-        tn.write(b"exit\n")
-        tn.write(b"exit\n")
-
-        print(tn.read_all().decode('ascii'))
+if __name__ == "__main__":
+    aiakos = Aiakos()
+    aiakos.run()
