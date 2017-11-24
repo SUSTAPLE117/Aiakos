e181449f51859e258cba28fd44b4ce24bbf81a41
Alexis-Maurer Fortin <sustaple_1@hotmail.com>
2017-11-24 12:10:15 -0500

    Class server with password generation method and basic run method

--- server/server.py
+++ server/server.py
@@ -1,6 +1,29 @@
 import http.server
 import ssl
+import string
+from secrets import choice
 
-httpd = http.server.HTTPServer(('localhost', 443), http.server.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile='', server_side=True)
-httpd.serve_forever()
+
+class AiakosServer(object):
+    certificate_file = None
+
+    def __init__(self, certificate_file):
+        self.certificate_file = certificate_file
+
+    def run(self):
+        httpd = http.server.HTTPServer(('localhost', 443), http.server.SimpleHTTPRequestHandler)
+        httpd.socket = ssl.wrap_socket(httpd.socket, certfile=self.certificate_file, server_side=True)
+        httpd.serve_forever()
+
+    @staticmethod
+    def generate_password(password_length):
+
+        alphabet = string.ascii_letters + string.digits
+        while True:
+            password = ''.join(choice(alphabet) for i in range(password_length))
+            if (any(c.islower() for c in password)
+                    and any(c.isupper() for c in password)
+                    and any(c.lower() for c in password)
+                    and sum(c.isdigit() for c in password) >= 3):
+                break
+        return password
